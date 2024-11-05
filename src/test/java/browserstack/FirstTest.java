1e9c4c021ac93be529b24c0ff50f17ea891ea267
IoT-Testing <130956852+IoT-Testing@users.noreply.github.com>
2024-11-05 15:40:14 +0530

    Browserstack is working

--- src/test/java/browserstack/FirstTest.java
+++ src/test/java/browserstack/FirstTest.java
@@ -7,6 +7,7 @@ import app.MoreTab.Manage;
 import app.ScreenCheck.ScreenCheck;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.ios.IOSDriver;
 import org.openqa.selenium.MutableCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -17,12 +18,12 @@ import java.time.Duration;
 
 public class FirstTest extends AppiumTest {
     public AndroidDriver driver;
-
-    @Test
+    public IOSDriver check;
+@Test
     public void test() throws Exception {
         Manage manage = new Manage(driver);
         ScreenCheck screen = new ScreenCheck(driver);
-        Email.Login(driver);
+        Email.Login(check);
         Monkey.run(driver);
     }
 
