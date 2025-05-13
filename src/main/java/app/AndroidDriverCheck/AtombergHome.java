4ee3e12faf1efb0ad9434a318d58717dd0e41b07
IoT-Testing <130956852+IoT-Testing@users.noreply.github.com>
2025-05-13 09:15:44 +0530

    operating properly

--- src/main/java/app/AndroidDriverCheck/AtombergHome.java
+++ src/main/java/app/AndroidDriverCheck/AtombergHome.java
@@ -10,7 +10,6 @@ import app.util.ActionsUtil;
 import app.util.AppUtil;
 import app.util.PermissionUtil;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
