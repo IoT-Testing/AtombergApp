d45237d2cf29b5636cf27426c08a72cec18e093b
IoT-Testing <130956852+IoT-Testing@users.noreply.github.com>
2025-06-20 13:33:09 +0530

    removed irrelevant files and packages

--- src/test/java/Tests/Monkey.java
+++ src/test/java/Tests/Monkey.java
@@ -1,7 +1,7 @@
 package Tests;
 
 import AtombergTest.Method;
-import Login.Email;
+import app.WaterPurifier.Login.Email;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.appium.java_client.android.AndroidDriver;
