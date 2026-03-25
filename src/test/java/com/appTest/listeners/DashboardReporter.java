71754ce19f03ab396e0559ab6358e7f5edf9dffd
IoT-Testing <iot.testing@atomberg.com>
2026-03-25 15:07:01 +0530

    check

--- src/test/java/com/appTest/listeners/DashboardReporter.java
+++ src/test/java/com/appTest/listeners/DashboardReporter.java
@@ -1,10 +1,12 @@
 package com.appTest.listeners;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.*;
 import com.appTest.models.AppiumTestResult;
 import com.google.common.net.MediaType;
 import org.junit.runner.Request;
 import org.testng.*;
+import tools.jackson.databind.ObjectMapper;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
